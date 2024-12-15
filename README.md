edbbceecfb25878d342369a108bf352a609c12da
alexzheng111 <alexzhengalt@gmail.com>
2024-12-15 11:03:15 -0600

    Create dummy fastapi

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This repository holds all python learnings.
 ## Testing
 
 `poetry run pytest`
+
+## FastAPI
+
+Run the dev server with `fastapi dev fastapi_tutorial/main.py`.
---
+++ fastapi_tutorial/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi_tutorial.models.user import User
+
+app = FastAPI()
+
+@app.get("/hello")
+def read_hello():
+    return {"message": "Hello World!"}
+
+@app.get("/users/{user_id}")
+def get_user() -> User:
+    return {
+        "id": "asdf",
+        "email": "alexzhengalt@gmail.com",
+        "hashed_password": "asdfasdfa",
+        "asdfa": 1 # Gets stripped out
+    }
