371564ea8fdadc0c60e9847f7a37b3351f0455d5
alexzheng111 <alexzhengalt@gmail.com>
2024-12-18 21:12:47 -0600

    lint

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -22,5 +22,6 @@ def get_user() -> User:
         "asdfa": 1,  # Gets stripped out
     }
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
